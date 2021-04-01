aef28200c92aee95ae9b40f5253f97c17eb55403
tusharpatil3688 <tusharpatil3688@gmail.com>
2021-04-01 14:34:57 +0530

    fix:deleted custom doctype payment request

--- alqlab_customization/alqlab/doctype/payment_request/payment_request.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, bizmap technologies,pune and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Payment Request', {
-	// refresh: function(frm) {
-
-	// }
-});
